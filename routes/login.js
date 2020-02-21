9afc666b8b68706881eca347193f4e05e848bd43
nyangule <nyanguleo@gmail.com>
2020-02-20 19:37:02 -0800

    correct

--- routes/login.js
+++ routes/login.js
@@ -1,112 +1,52 @@
+
+const mysql = require('mysql');
 const express = require('express');
-router = express.Router();
-con = require('../conn/conn');
+
+const mysqlConn= require('../conn/conn');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+	
+/*router.get('/login', function(request, response) {
+	
+	var email = request.body.email;
+	var pwd = request.body.pwd;
+	
+
+	
+		mysqlConn.query('SELECT   email , pwd  from landlord ', [email, pwd], function(error, results, fields) {
+            if (!error) {
+                response.send({data:results});
+                
+			}else {
+				response.send(error)
+			}			   
+           })
+					   
+			
+		})*/
+		router.get('/login', function(request, response) {
+	
+			var email = request.body.email;
+			var pwd = request.body.pwd;
+			
+		var sql = "SELECT * FROM landlord Where email = ? AND pwd = ?";
+			
+				mysqlConn.query(sql, [email, pwd], function(error, results, fields) {
+					if (!error) {
+						response.send({data:results});
+						Message.send("successfully logged");
+						
+					}else {
+						response.send(error)
+					}			   
+				   })
+							   
+					
+				})
+	
 
 
-
-   router.get('/ll', (req,res)=>{
-
-    let email =( req.body.email)
-
-   con.query('SELECT * from landlord where email = ? ',[email],function(error,results,fields){
-    
-       if(error){
-         return res.send("error in  the query")
-       }
-       else{
-        
-               return res.send({data:results})
-           }
-
-       })
-
-
-     router.get('/st', (req,res)=>{
-    let email=( req.body.email)
-
-   
-     con.query('SELECT * from student where email= ? ',[email],function(error,results,fields){
-    
-       if(error){ throw(error)
-         
-       }
-       else{
-        
-               return res.send({data:results})
-           }
-       })
-
-
-       /*app.get('/logout', (req, res, next) => {
-        req.logOut()
-        console.log('Youve been logged out'),
-        res.redirect('/login')*/
-   
-   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-     /*  router.post('/insert',function(res,post){
-connection.query("insert INTO studentTbl SET ? " ,res.body,function(err,rs){
-res.render('insert success') fokza
-})
-
-       })
-
-
-      /* app.get('/logout', (req, res, next) => {
-        req.logOut()
-        console.log('Youve been logged out'),
-        res.redirect('/login')*/
-   
-  })
-
-
-
-   /*router.delete('/delete',function(req,res){
-
-    var email = req.body.email;
-
-    var sql = "DELETE FROM studentTbl WHERE email = ?";
-   con.query(sql, [email], function (err, result) {
-     if (err) throw err;
-     console.log("Number of records deleted: " + result.affectedRows);
-   });
-
-
-   })
-
-
-   //Update student table
-/*router.post('/update',function(req,res){
-    var name = req.body.name;
-    var qty = req.body.qty;
-  
-    //Udate qty
-    var sql="UPDATE studentTbl SET email = ? WHERE password = ? " ;
-    mysqlConn.query(sql,[email,password] ,function (err, results,fields){
-        if(!err){
-                    
-                    res.send(results)
-                }else{
-                    console.log(err)
-                }
-     });
-    
-  })*/
-  
-
-   
 
 
 module.exports = router ;
